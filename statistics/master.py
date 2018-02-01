1f881f61f738a464ac2d5b47b48d231cf5a0b3cf
erikvanhaeringen <e.s.van.haeringen@student.rug.nl>
2018-02-01 17:31:32 +0100

    not working yet

--- statistics/master.py
+++ statistics/master.py
@@ -12,9 +12,10 @@ return output
 """
 import re
 import pandas as pd
+import subprocess
 
 def main(ResultDf,PlasmidCount,ArgCount,PlasmidDf,ArgDf,RelativePath):
-
+    #if called from the main master script add the relative path to all used file paths
     PathAddition=""
     if RelativePath:
         PathAddition = RelativePath
@@ -22,17 +23,19 @@ def main(ResultDf,PlasmidCount,ArgCount,PlasmidDf,ArgDf,RelativePath):
     print "\n\n[ Preparing data for statistical analysis ]"
     #build a subdataset that shows the time a specific was found arg per plasmid type, with the last row showing the arg count with no plasmid type
     
-    #generate a list of plasmid types with the subtype information removed for further analysis
+
     print "parsing plasmid type IDs"
+    #generate a list of plasmid types with the subtype information removed for further analysis
     PlasmidIDs = []
 
-    for PlasmidFile in range(len(PlasmidDf)):
+    #iterate through the plasmid type sequences and perform a search and replace with regular expressions (re)
+    for PlasmidFile in range(len(PlasmidDf)): 
         for Plasmid in range(len(PlasmidDf[0][PlasmidFile])):
             PlasmidIDs.append(re.sub(r"^([A-Za-z0-9/]+).*",r"\1",PlasmidDf[0][PlasmidFile][Plasmid]))
-    PlasmidIDs.append("NoType")
-
-    #generate a list of arg types with the subtype information removed for further analysis   
+    PlasmidIDs.append("NoType") #add a 'no type' row the plasmid types list to store all resistance matches without a plasmid type match
+ 
     print "parsing resistance type IDs"
+    #generate a list of arg types with the subtype information removed for further analysis  
     ArgIDs = []
     for ArgFile in range(len(ArgDf)):
         for Arg in range(len(ArgDf[0][ArgFile])):
@@ -42,121 +45,53 @@ def main(ResultDf,PlasmidCount,ArgCount,PlasmidDf,ArgDf,RelativePath):
     print "create new data set"
     #Create empty dataset with plasmid type as row index and arg type as column headers            
     SubDataSet = pd.DataFrame(columns=(ArgIDs)) #sets column names
+    
+    #loop that fills the data set with 0's for the number of plasmid types (including no type) times the number of resistance gene types
     for PlasmidNumber in range(PlasmidCount+1): #(+1 for notype row)
         SubDataSet.loc[PlasmidNumber] = [0]*ArgCount
-    SubDataSet.index = PlasmidIDs #sets row names
+    SubDataSet.index = PlasmidIDs #sets row names based on the shortened plasmid type names 
     
     print "transpose data to new data set"
+    #fill the new data set with data from the output of the analysis step
+    #using a loop that iterates through the samples 
     for Sample in range(len(ResultDf)):
+        #and per sample checks if any plasmid type was found
+        #if not it adds the antibiotic types count to the 'no type' row
         if sum([int(i) for i in ResultDf.loc[Sample][2:(PlasmidCount + 2)]]) == 0: #no plasmid type identified in sample          
             for ArgNumber in range(ArgCount):
-                if ResultDf.loc[Sample][2+PlasmidCount+ArgNumber]=="1":
-                    SubDataSet.loc[PlasmidIDs[PlasmidCount],ArgIDs[ArgNumber]]+=1
+                if ResultDf.loc[Sample][2+PlasmidCount+ArgNumber]=="1": #uses string type as the output data set is all strings
+                    SubDataSet.loc[PlasmidIDs[PlasmidCount],ArgIDs[ArgNumber]]+=1 #add to new dataset
+        #if so it adds the antibiotic types count to the row of the plasmid type(s) that were found
         else:
             for Plasmid in len(ResultDf.loc[Sample][2:(PlasmidCount+2)]):
                 if ResultDf.loc[Sample][2+Plasmid] == "1":
                     for ArgNumber in range(ArgCount):
-                        if ResultDf.loc[Sample][2+PlasmidCount+ArgNumber]=="1":
-                            SubDataSet.loc[PlasmidIDs[Plasmid],ArgIDs[ArgNumber]]+=1
+                        if ResultDf.loc[Sample][2+PlasmidCount+ArgNumber]=="1": #uses string type as the output data set is all strings
+                            SubDataSet.loc[PlasmidIDs[Plasmid],ArgIDs[ArgNumber]]+=1 #add to new dataset
 
 
     #export the subdataset for r to a tab delimited file with column and row names included
     outputFile = PathAddition+"results/SubDataSet.txt"
     with open(outputFile, 'w') as file:
-        file.writelines('\t'+ Arg for Arg in ArgIDs)
-        file.writelines("\n")
-        PlasmidCounter = 0
-        for index, row in SubDataSet.iterrows():
+        file.writelines('\t'+ Arg for Arg in ArgIDs) #header with the column names
+        file.writelines("\n") #line break for the header line
+        #iterate through each item in the data set and add it to a temporary string, separated by tabs
+        for Plasmid in range(PlasmidCount):
             TempStr=""
-            for Arg in row.iteritems():
-                if TempStr=="":
-                    TempStr=PlasmidIDs[PlasmidCounter] + "\t" + str(Arg[1])
+            for Arg in range(ArgCount):
+                #determin if it is the first item in the row, if so also add the row name
+                if TempStr=="": 
+                    TempStr=PlasmidIDs[Plasmid] + "\t" + str(SubDataSet.loc[PlasmidIDs[Plasmid],ArgIDs[Arg]]) #add row name (plasmid type) and item to temp string preceded by a tab
                 else:
-                    TempStr = TempStr + "\t" + str(Arg[1])
-            file.writelines(TempStr + "\n")
-            PlasmidCounter+=1
-    print "Exported plasmid versus arg dataset to", outputFile   
+                    TempStr = TempStr + "\t" + str(SubDataSet.loc[PlasmidIDs[Plasmid],ArgIDs[Arg]])
+            file.writelines(TempStr + "\n") #write the temporary string to the file plus a line break and continue to the next line (plasmid type)
+    print "Exported transposed dataset for r to", outputFile #show user where the file is saved to
+    
+    print "\n[ STATISTICAL ANALYSIS WITH R SCRIPT ]"
+    RScriptPath = PathAddition+"/scripts/summarize.r"
+    print "Running", RScriptPath
+    subprocess.call (["/usr/bin/Rscript", "--vanilla", RScriptPath])
+    
     
     return SubDataSet
-
-#import re
-#
-#def main(DataSet,PlasmidCount,ArgCount,PlasmidData,ArgData,RelativePath):
-#    PathAddition=""
-#    if RelativePath:
-#        PathAddition = RelativePath
-#        
-#    #build a subdataset that shows the time a specific was found arg per plasmid type, with the last row showing the arg count with no plasmid type
-#    SubDataSet = [[0 for col in range(2264)] for row in range(271)]
-#    for Row in DataSet:
-#        if sum([int(i) for i in Row[2:(PlasmidCount + 2)]]) == 0: #no plasmid type identified in sample
-#            for ArgNumber in range(ArgCount):
-#                if Row[(2+PlasmidCount+ArgNumber)]=="1":
-#                    SubDataSet[270][ArgNumber]+=1
-#        else:
-#            PlasmidNumber = 0
-#            for Plasmid in Row[2:(PlasmidCount + 2)]:
-#                if Plasmid == "1":
-#                    for ArgNumber in range(ArgCount):
-#                        if Row[(2+PlasmidCount+ArgNumber)]=="1":
-#                            SubDataSet[PlasmidNumber][ArgNumber]+=1
-#                PlasmidNumber+=1
-#            
-#    #generate a list of plasmid types with subtype information removed for analysis
-#    PlasmidIDs = []
-#    for PlasmidFile in PlasmidData:
-#        for PlasmidType in PlasmidFile:
-#            PlasmidIDs.append(re.sub(r"^([A-Za-z0-9/]+).*",r"\1",PlasmidType[0]))
-#    PlasmidIDs.append("NoType")
-#    
-#    #generate a list of arg types with subtype information removed for analysis    
-#    ArgIDs = []
-#    for ArgFile in ArgData:
-#        for ArgType in ArgFile: 
-#            TempReplace = re.sub(r"([A-Za-z0-9()]+).+",r"\1",ArgType[0])
-#            ArgIDs.append(re.sub(r"(.+)\w\d$",r"\1",TempReplace))
-#            
-#    #export the subdataset for r to a tab delimited file with column and row names included
-#    outputFile = PathAddition+"results/SubDataSet.txt"
-#    with open(outputFile, 'w') as file:
-#        file.writelines('\t'+ Arg for Arg in ArgIDs)
-#        file.writelines("\n")
-#        PlasmidCounter = 0
-#        for PlasmidType in SubDataSet:
-#            TempStr=""
-#            for Arg in PlasmidType:
-#                if TempStr=="":
-#                    TempStr=PlasmidIDs[PlasmidCounter] + "\t" + str(Arg)
-#                else:
-#                    TempStr = TempStr + "\t" + str(Arg)
-#            file.writelines(TempStr + "\n")
-#            PlasmidCounter+=1
-#    print "Exported plasmid versus arg dataset to", outputFile
-#    
-#    
-#    
-#    
-#    
-#    
-#    
-#    #r part
-##    from numpy import *
-##    import scipy as sp
-##    from pandas import *
-##    from rpy2.robjects.packages import importr
-##    import rpy2.robjects as ro
-##    import pandas.rpy.common as com
-##
-##    rdf = com.convert_to_r_dataframe(df)    
-##        
-#    import pandas as pd
-#    PandaDataSet = pd.DataFrame(columns=(ArgIDs)) #sets column names
-#    for PlasmidNumber in range(len(SubDataSet)):
-#        PandaDataSet.loc[PlasmidNumber] = SubDataSet[PlasmidNumber]
-#    PandaDataSet.index = PlasmidIDs #sets row names
-#
-#    
-#   
-#    
-#    
-#    return SubDataSet
+    
---
+++ statistics/scripts/summarize.r
@@ -0,0 +1,104 @@
+#! /usr/bin/Rscript
+
+#---
+#title: "PlasmidProject"
+#author: "Goncalo"
+#date: "January 30, 2018"
+#output: html_document
+#editor_options: 
+#  chunk_output_type: console
+#---
+
+
+
+# read data, read libraries, clean data to use
+
+#```{r fig.width=7, fig.height=6}
+
+# Define working directory
+#setwd("C:/Users/gonca/surfdrive/PhD/R/PlasmidProject")
+
+
+# Check/confirm directoty
+#getwd()
+
+# Load needed libraries needed (package)
+#install.packages("ggplot2")
+#install.packages("dplyr")
+
+#library(ggplot2) # this is the main library for clean and easy plotting. 
+library(dplyr)
+library(data.table)
+
+#library(doBy)
+#library(reshape2)
+#library(plyr)
+#library(tidyr)
+#library(vegan)
+
+
+# Load dataset (make sure to change all spaces, empty cells and strange characters)
+alldata<-read.table("../results/SubDataSet.txt", sep="\t", header=TRUE,  na.strings=c("", "NA"))
+
+
+# Confirm consistency of data loaded/generated
+#str(alldata)
+#ncol(alldata)
+#nrow(alldata)
+#summary(alldata)
+
+
+# Data manipulations
+
+alldata_treated <- alldata
+#colnames(alldata_treated)
+
+#check caracteristics of new dataset
+#str(alldata_treated)
+#ncol(alldata_treated)
+#nrow(alldata_treated)
+
+
+# make new variables (family of antibiotic) based on sum of different genes of that family
+
+alldata_treated$TET <- apply(alldata_treated[,c(2:105)], 1, sum) #tetracycline
+
+alldata_treated$BLA <- apply(alldata_treated[,c(106:1489)], 1, sum) #Beta-lactams
+
+alldata_treated$MAC <- apply(alldata_treated[,c(1490:1621)], 1, sum) #Macrolide
+
+alldata_treated$NIM <- apply(alldata_treated[,c(1622:1635)], 1, sum) #Nitroimidazole
+
+alldata_treated$COL <- apply(alldata_treated[,c(1636:1655)], 1, sum) #colistin
+
+alldata_treated$FOS <- apply(alldata_treated[,c(1656:1696)], 1, sum) #fosfomycin
+
+alldata_treated$AMN <- apply(alldata_treated[,c(1698:1865)], 1, sum) #Aminoglycoside
+
+alldata_treated$QNL <- apply(alldata_treated[,c(1866:1968)], 1, sum) #Quinolone
+
+alldata_treated$PHN <- apply(alldata_treated[,c(1969:2002)], 1, sum) #Phenicol
+
+alldata_treated$RIF <- apply(alldata_treated[,c(2007:2015)], 1, sum) #rifampicin
+
+alldata_treated$VAN <- apply(alldata_treated[,c(2016:2038)], 1, sum) #vancomycin
+
+alldata_treated$SUL <- apply(alldata_treated[,c(2162:2210)], 1, sum) #sulphonamide
+
+alldata_treated$FUS <- apply(alldata_treated[,c(2211:2212)], 1, sum) #fusidic acid
+
+alldata_treated$TRM <- apply(alldata_treated[,c(2213:2265)], 1, sum) #trimethprim
+
+
+# New table with only sums of ARG, by antibiotic class
+alldata_final <- alldata_treated[,c(1,2266:2279)]
+str(alldata_final)
+
+# remove rows that are all zeros
+#alldata_final <- alldata_final[!!rowSums(abs(alldata_final[-c(1:2)])),]
+#str(alldata_final)
+
+
+alldata_final
+
+
